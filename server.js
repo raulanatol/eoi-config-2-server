7bb2543945b1c4d65582f7e109c339d382dbd47d
raulanatol <raul@natol.es>
2022-04-13 20:28:47 +0100

    Update

--- server.js
+++ server.js
@@ -1,14 +1,26 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
-const port = 3000;
+const port = 3001;
 
+const fs = require('fs');
+
+
+
+/**
 const users = [
   { name: 'Marta' },
   { name: 'Paco'  },
-  { name: 'Lucas' }
+  { name: 'Lucas' },
+  { name: 'Lucía' },
+  { name: 'Mateo' }
 ];
+**/
+
+app.use(cors());
 
 app.get('/users', (req, res) => {
+  const users = JSON.parse(fs.readFileSync('/tmp/users.json', 'utf-8'));
   res.json(users);
 });
 
